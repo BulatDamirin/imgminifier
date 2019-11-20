94c05900fb7c090a86ebce171850cf5039ffd6af
BulatDamirin <BulatDamirin@yandex.ru>
2019-11-20 11:47:47 +0300

    first

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,74 @@
-const gulp  = require('gulp'),
-	  nokey = require('gulp-tinypng-nokey');
-const imagemin = require("gulp-imagemin");
-const webp = require("imagemin-webp");
+const gulp       = require('gulp');
+const nokey      = require('gulp-tinypng-nokey');
+const imagemin   = require("gulp-imagemin");
+const webp       = require("imagemin-webp");
 const extReplace = require("gulp-ext-replace");
+const watermark  = require("gulp-watermark");
+const gm         = require('gulp-gm');
+const imageResize = require('gulp-image-resize');
 
-gulp.task('default', function() {
-	return gulp.src('img/**/*')
+
+const mainFolder = 'img/**/*';
+
+const forCompressFolder = 'compress';
+const forWebpFolder = "webp";
+const forWatermarkFolder = "watermark-complete-img";
+const generalFolder = "general";
+
+const serviceFolders = {
+	self: "service-folders/",
+	temporaryFolder: this.self + "temporaryFolder",
+	watermarks: this.self + "watermarks",
+}
+
+
+gulp.task('compress', function() {
+	return gulp.src(mainFolder, {base:"."})
 		.pipe(nokey())
-		.pipe(gulp.dest('miniImg'));
+		.pipe(gulp.dest(forCompressFolder));
 });
 
+gulp.task('resize', function() {
+	return gulp.src(mainFolder, {base:"."})
+		.pipe(imageResize({
+			width : 1200,
+			height: 1200,
+			crop : false,
+			upscale : false
+		}))
+		.pipe(gulp.dest(forCompressFolder));
+})
 
+gulp.task('compress-resize', function() {
+	return gulp.src(mainFolder, {base:"."})
+		.pipe(imageResize({
+			width : 1200,
+			crop : false,
+			upscale : false
+		}))
+		.pipe(nokey())
+		.pipe(gulp.dest(forCompressFolder));
+})
 
-gulp.task("imagemin", function() {
-  let src = "miniImg/**/*"; // Where your PNGs are coming from.
-  let dest = "dist"; // Where your WebPs are going.
-
-  return gulp.src(src)
+gulp.task("webp", function() {
+  return gulp.src(mainFolder, {base:"."})
     .pipe(imagemin([
       webp({
         quality: 100
       })
     ]))
     .pipe(extReplace(".webp"))
-    .pipe(gulp.dest(dest));
+    .pipe(gulp.dest(forWebpFolder));
 });
+
+gulp.task("watermark", function() {
+	return gulp.src(mainFolder)
+		.pipe(watermark({
+			image: serviceFolders.watermarks + '/logo.png',
+			resize: '60%',
+			gravity: 'North',
+			dissolve: 30
+		}))
+		
+		.pipe(gulp.dest(forWatermarkFolder));
+})
